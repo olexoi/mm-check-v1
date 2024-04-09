7f65deab72980d0ebdc4604bef5d791a544b6d24
olexoi <opoplavskiy64@gmail.com>
2024-04-10 00:09:41 +0300

    ww

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = express()
-const port = 3000
+const port = 8080
 const apiRoutes = express.Router()
 
 app.use(cors())
